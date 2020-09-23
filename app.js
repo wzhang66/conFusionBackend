cdaedb60c5d2b58a14331ec74b823b0889ee0cae
Weiwei <ww.zhang@mail.utoronto.ca>
2020-09-23 13:30:28 -0400

    Basic authentication

--- app.js
+++ app.js
@@ -47,7 +47,19 @@ function auth(req, res, next){
     return next(err);
   }
 
-  var auth = new Buffer(authHeader,)
+  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+
+  var username = auth[0];
+  var password = auth[1];
+
+  if(username === 'admin' && password === 'password') {
+    next();
+  } else {
+    var err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
 }
 
 app.use(auth);
